1305e90e96ac226707a560a3d5620efc493a956a
rishabhtech1994 <rishi.gupta8819@gmail.com>
2023-10-07 16:47:36 +0530

    Initial commit

---
+++ src/main/kotlin/classicaldesignpattern/creationaldesignpattern/Singleton.kt
@@ -0,0 +1,18 @@
+package classicaldesignpattern.creationaldesignpattern
+
+object Singleton{
+    init {
+        println("Singleton called")
+    }
+    val name = "My name is Rishabh"
+    fun add( num: Int , num1: Int): Int{
+        return  num + num1
+    }
+}
+
+fun main() {
+    println("Singleton ${Singleton.name}")
+    println("add called ${Singleton.add(5,10)}")
+    println("add called ${Singleton.add(30,50)}")
+
+}
